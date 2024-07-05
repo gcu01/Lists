04d38dc5bdb84781bce1da181bdea3e92bc64f49
Gabriel <gabrielcampeanu@gmail.com>
2024-07-05 10:53:26 +0200

    first version of the list and unit tests

--- linked_lists/src/first.rs
+++ linked_lists/src/first.rs
@@ -1,3 +1,5 @@
+use std::{mem};
+
 #[derive(Debug)]
 pub struct List {
     head: Link,
@@ -20,7 +22,31 @@ impl List {
         List{head: Link::Empty}
     }
 
+    //push is adding a new elem on the head of the list
+    // list             a->b->null   where a is the head
+    // push(c) on list: c->a->b->null where c is the new head
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+                            elem:elem,
+                            next:mem::replace(&mut self.head, Link::Empty),
+                        });
 
+    self.head = Link::More(new_node);
+    }
+
+    //pop is removing the elem from the head of the list
+    pub fn pop(&mut self) -> Option<i32> {
+        let result;
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => result = None,
+            Link::More(node) => {
+                self.head = node.next;
+                result = Some(node.elem);
+            }
+        };
+        //unimplemented!()
+        result
+    }
 }
 
 
@@ -29,8 +55,34 @@ impl List {
 
 
 
+#[cfg(test)]
+mod test {
+    use super::*;
 
+    #[test]
+    fn basics() {
+        let mut list = List::new();
 
+        assert_eq!(None, list.pop());
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(Some(3), list.pop());
+        assert_eq!(Some(2), list.pop());
+
+        list.push(4);
+        list.push(5);
+
+        assert_eq!(Some(5), list.pop());
+        list.pop();
+        list.pop();
+
+        assert_eq!(None, list.pop())
+
+    }
+}
 
 
 
@@ -45,7 +97,7 @@ pub struct Test {
 }
 
 impl Test {
-    pub fn new() -> Test { //we can use -> Self as well, be carrefoul, Self is borrowing the value
+    pub fn new() -> Test { //we can use -> Self (with capital S because it represents the type) as well, be carrefoul, Self is borrowing the value
         Test{first:0, second:0}
     }
 }
